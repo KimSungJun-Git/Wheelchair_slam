56324fbe27a8febbe627907928ecf39459c44e5a
KimSungJun-Git <ksjun100848@naver.com>
2026-03-28 10:36:19 +0900

    첫 번째 저장

--- README.md
+++
@@ -1,7 +0,0 @@
-Robot platform : N2
-
-LIDAR : YDLIDAR x4 or YDLIDAR x4 Pro
-
-ROS ver : ROS2 Humble
-
-Where to use : Computer
---
+++ wheelchair_robot_control/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os # 1. os 임포트 추가
+from glob import glob # 2. glob 임포트 추가
+
+package_name = 'wheelchair_robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kim',
+    maintainer_email='ksjun100848@naver.com',
+    description='Wheelchair Robot Control Package with EKF',
+    license='Apache 2.0',
+    extras_require={
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'main_controller = wheelchair_robot_control.main_controller:main',
+        ],
+    },
+)
